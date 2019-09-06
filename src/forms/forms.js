5eb09f674be160cb278d2e8bcd59b826bf8dc760
Emanuel <esanga@live.com>
2019-09-06 13:02:56 +0300

    configured the inout component to receive data from the form component

--- src/forms/forms.js
+++ src/forms/forms.js
@@ -1,9 +1,7 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Input from "./inputs";
 
 export default class Form extends Component{
-    constructor(props) {
-        super(props);
-    }
     componentDidMount() {
         console.log("component mounted")
     }
@@ -11,7 +9,22 @@ export default class Form extends Component{
     render() {
         return (
             <div>
-                {this.props.properties}
+                <Input
+                    className={'pa3 ba b--green bg-lightest-blue'}
+                    input_type={`text`}
+                    placeholder={`inputs`}
+                />
+                <Input
+                    className={'pa3 ba b--green bg-lightest-blue'}
+                    input_type={`textarea`}
+                    placeholder={`inputs`}
+                />
+                <Input
+                    className={'pa3 ba b--green bg-lightest-blue'}
+                    input_type={`button`}
+                    value={`submit`}
+                    placeholder={`inputs`}
+                />
             </div>
         );
     }
--- src/forms/inputs.js
+++ src/forms/inputs.js
@@ -1,5 +1,25 @@
 import React from 'react';
 
-const input = (props) => {
-
+const Input = (props) => {
+    let inputElement = null;
+    switch(props.input_type) {
+        case ('text'):
+            inputElement = <input type={`text`} {...props}/>;
+            break;
+        case ('textarea'):
+            inputElement = <textarea {...props}/>;
+            break;
+        case ('button'):
+            inputElement = <input type={`button`} {...props}/>;
+            break;
+        default:
+            inputElement = "no form specified!!"
+    }
+    return (
+        <div>
+            { inputElement }
+        </div>
+    );
 };
+
+export default Input;
