09902848fb8526a6264471a98b2393dbd3b98420
Emanuel Sanga <esanga530@gmail.com>
2019-10-23 20:51:26 +0300

    done with all the importations of redux into my App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux'
 import Cardlist from './Cardlist';
 import Searchbox from './Searchbox';
 import ErrorBoundry from './ErrorBoundry'
 import Scrollable from './Scrollable'
 import robots from './robots'
 import './App.css'
+// eslint-disable-next-line no-unused-vars
+import {searchRobots} from "./reducers";
+import {setSearchField} from "./actions";
 
+const mapStateToProps = state => {
+    return {
+        searchField: state["searchRobots"].searchField
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+}
 
 class App extends Component{
     constructor(){
@@ -21,7 +36,6 @@ class App extends Component{
     };
 
     componentDidMount() {
-        console.log(this.props.store.getState());
         fetch('https://jsonplaceholder.typicode.com/users')//fetch it from an online API
             .then(response => response.json())//convert it into json
             .then(users => this.setState({robots: users}))//assign it to robots
@@ -52,4 +66,4 @@ class App extends Component{
     }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
 import './index.css';
 import './Card.css';
 import App from './App';
 import 'tachyons';
 import * as serviceWorker from './serviceWorker';
 /* redux's part*/
+import {applyMiddleware, createStore} from 'redux';
 import {searchRobots} from "./reducers";
 import {Provider} from "react-redux";
+import {createLogger} from 'redux-logger';
 
-const store = createStore(searchRobots);
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger));
 
 ReactDOM.render(
     <Provider store={ store }>
-        <App/>
+        <App />
     </Provider>
-
     , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
